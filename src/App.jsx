ab1b77c2296e0f4441c990ec990a190e9630e6cf
Deepakkumarrp <deepakkumarrp@gmail.com>
2024-01-20 00:37:13 +0530

    Add Routes and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import viteLogo from '/vite.svg'
 import { AddressDetails } from './Components/Payment/Address'
 // import './App.css'
 import Products from './Components/Products/Products'
+import AllRoutes from './Components/AllRoutes'
+// import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <>
-    <AddressDetails />
-      {/* <Paymentdetails/> */}
-      <Products />
+      {/* <Navbar /> */}
+      <AllRoutes />
+      <Footer />
     </>
   )
 }
