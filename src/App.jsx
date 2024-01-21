57c0f7955b4e3a505a9cb153c21ce9b4f511383d
PoornimaMohabe <147687634+PoornimaMohabe@users.noreply.github.com>
2024-01-22 01:46:39 +0530

    Revert "all features added"

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-
+import { AddressDetails } from './Components/Payment/Address'
 // import './App.css'
 import Products from './Components/Products/Products'
 import { Routes } from 'react-router-dom'
